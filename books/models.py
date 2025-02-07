bd8abfcf651610042210d8c1b2c47f097010acd4
Moki008 <moki36083@gmail.com>
2025-02-07 19:36:40 +0600

    'Update'

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class BookModel(models.Model):
+    GENRE_CHOICES = (
+        ('History', 'History'),
+        ('Mystery', 'Mystery'),
+        ('Romance', 'Romance'),
+        ('Fantasy', 'Fantasy'),
+    )
+    image = models.ImageField(upload_to='books/', verbose_name='Загрузите фото')
+    title = models.CharField(max_length=100, verbose_name='Укажите название книги')
+    description = models.TextField(verbose_name='Напишите краткое описание книги', blank=True)
+    price = models.PositiveIntegerField(verbose_name='Укажите цену', blank=True)
+    release_date = models.DateField(auto_now_add=True)
+    genre = models.CharField(max_length=10, choices=GENRE_CHOICES, default='None', verbose_name='Укажите жанр')
+    mail_author = models.CharField(max_length=100, default='None',verbose_name='Укажите почту автора')
+    author = models.CharField(max_length=100, default='None', verbose_name='Напишите имя автора')
+    video = models.URLField(verbose_name='Укажите ссылку из YouTube')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'книгу'
+        verbose_name_plural = 'книги'
--- books/views.py
+++ books/views.py
@@ -1,18 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from datetime import datetime
+from . import models
+
+
+def book_list_view(request):
+    if request.method == 'GET':
+        query = models.BookModel.objects.all()
+        context_object = {
+            'book': query
+        }
+        return render(request, template_name='book.html', context=context_object)
+
+
+def book_detail_view(request, id):
+    if request.method == 'GET':
+        query = get_object_or_404(models.BookModel, id=id)
+        context_object = {
+            'book_id': query
+        }
+        return render(request, template_name='book_detail.html', context=context_object)
+
 
 def about_me(request):
     if request.method == 'GET':
         return HttpResponse('Я учусь на бэкенде')
+
 
 def text_and_photo(request):
     if request.method == 'GET':
         return HttpResponse('<h1>Cat</h1> '
                             '<img src="https://www.ferra.ru/imgs/2024/05/08/05/6460496/c2150453d059e8999c5f0b211ce334f7c869147c.jpg">')
 
+
 def system_time(request):
     if request.method == 'GET':
         current_time = datetime.now()
         return HttpResponse(current_time)
-
